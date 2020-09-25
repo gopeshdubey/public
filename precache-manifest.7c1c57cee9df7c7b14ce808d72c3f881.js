7b5ab0dfe2d4396f89cb14fc2f6e5f11431641b6
gopeshdubey <gdubey96.gd@gmail.com>
2020-09-26 01:03:53 +0530

    first commit

---
+++ precache-manifest.7c1c57cee9df7c7b14ce808d72c3f881.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a58715d4285f3ef2e7e7da875183b1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe5fd3dd984d57d1ea2f",
+    "url": "/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "40fd4fd3a6cd18aa49b0",
+    "url": "/static/css/main.26e34edf.chunk.css"
+  },
+  {
+    "revision": "fe5fd3dd984d57d1ea2f",
+    "url": "/static/js/2.e4776eaa.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/static/js/2.e4776eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40fd4fd3a6cd18aa49b0",
+    "url": "/static/js/main.4b5ab8cd.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
